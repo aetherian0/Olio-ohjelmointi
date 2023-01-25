1f6ac44f875eef0618b3b789c2af1b8e6a431af7
MiskaTup <83165152+MiskaTup@users.noreply.github.com>
2023-01-25 18:02:01 +0200

    Update tänäänon.py

--- game/minipelit/tänäänon.py
+++ game/minipelit/tänäänon.py
@@ -13,16 +13,20 @@ päivä = Päivämäärä.weekday()
 
 #Kertoo viikonpäivän
 viikon_päivät=["Maanantai", "Tiistai", "Keskiviikko","Torstai","Perjantai","Lauantai","Sunnuntai"]
+viikon_päivät.remove(viikon_päivät[päivä])
+print(viikon_päivät)
+
+
+
+
 #print(viikon_päivät[päivä])
 
-Random=random.sample(viikon_päivät, 3)
-lista1= str(Random)
-lista1=lista1.replace("[","")
-lista1=lista1.replace("]","")
+#Random=random.sample(viikon_päivät, 3)
+#lista1= str(Random)
+#lista1=lista1.replace("[","")
+#lista1=lista1.replace("]","")
 
 
 
-lista2 = str(viikon_päivät[päivä])
+#lista2 = str(viikon_päivät[päivä])
 
-
-print(lista1+", "+ "'"+lista2+"'")
