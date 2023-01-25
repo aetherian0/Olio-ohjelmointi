367032576d6410476251efad6168b65cca4ffd43
AetherianFI <saarinenotso@gmail.com>
2023-01-25 16:17:33 +0200

    Update tuplanoppa.py

--- game/minipelit/tuplanoppa.py
+++ game/minipelit/tuplanoppa.py
@@ -2,19 +2,25 @@ import random
 
 class Tuplanoppa: 
     def __init__(self): 
+        '''
+        attribuutit
+            otsikko : str, luokan nimi
+            min, max : int, generoitavan alueen rajat
+            noppa1, noppa 2 : int, noppien silmäluvut
+        '''
         self.otsikko = "Tuplanoppa" 
         self.min = 0 
         self.max = 6 
-        self.noppa1 = 0 
-        self.noppa2 = 0 
+        self.noppa1 = random.randint(min, max)
+        self.noppa2 = random.randint(min, max)
 
 
-    def tarkista(self, parameter): 
+    def tarkista(self, arvaus): 
         return False 
 
     def reset(self): 
         raise NotImplementedError 
 
     def tallennus(self): 
-        raise NotImplementedError 
+        pass
 
