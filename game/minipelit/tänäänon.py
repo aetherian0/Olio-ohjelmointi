d7df9c00bbd28accd9b164141d3e90067bfad5f1
AetherianFI <saarinenotso@gmail.com>
2023-01-26 02:00:06 +0200

    update tänäänon.py

--- game/minipelit/tänäänon.py
+++ game/minipelit/tänäänon.py
@@ -1,8 +1,37 @@
 import random
 from datetime import datetime
 
-class TänäänOn:
-    pass
+class Tänäänon:
+    def __init__(self):
+        '''
+        attribuutit
+            otsikko : str, luokan nimi ja kuvaava ohje
+            viikonpäivät : list, lista kaikista viikonpäivistä
+            päiväys : date, kokonainen päivämäärä numeroina
+            päivä_numerona : int, monesko päivä viikosta numerona
+            tänään : str, päivän nimi kirjaimina
+            arvausten_määrä : int, arvausten määrä
+        '''
+        self.otsikko = "Tänään on"
+        self.viikonpäivät = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        self.päiväys = datetime.now()
+        self.päivä_numerona = Päivämäärä.weekday()
+        self.tänään = self.päiväys.strftime("%A")
+        self.arvausten_määrä = 0
+        
+   
+    def tarkista(self, arvaus):
+        '''
+        Tarkistaa onko arvattu viikonpäivä oikea sekä kasvattaa arvausten määrää yhdellä
+        '''
+        self.arvausten_määrä += 1
+        return arvaus == self.tänään
+
+    def reset(self):
+        pass	
+
+    def tallennus(self):
+        pass
 
 
 
