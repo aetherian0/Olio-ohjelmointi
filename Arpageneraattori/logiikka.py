5fc64172396cb3215f47e1b75e520600aa6f4f7d
AetherianFI <saarinenotso@gmail.com>
2023-04-19 16:04:28 +0300

    Update logiikka.py

--- Arpageneraattori/logiikka.py
+++ Arpageneraattori/logiikka.py
@@ -1,3 +1,9 @@
+import time
+
 class Arpageneraattori:
     def __init__(self):
-        self.id = 0
+        self.id = round(time.time())
+
+    def generoi_arpa(self):
+        arpa = open("Arpageneraattori/arvat/arpa_" + str(self.id) + ".txt" ,"x")
+        time.sleep(1)
